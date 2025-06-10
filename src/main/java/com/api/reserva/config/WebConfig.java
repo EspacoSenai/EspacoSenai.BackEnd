cf988f47c4f3b114abfe318f639863086ab092ba
SFullBox <fernando.limadsilva000@gmail.com>
2025-06-10 14:36:57 -0300

    Att: Modificação no formato da resposta

--- src/main/java/com/api/reserva/config/WebConfig.java
+++ src/main/java/com/api/reserva/config/WebConfig.java
@@ -1,11 +1,19 @@
 package com.api.reserva.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.ignoreAcceptHeader(true)
+                .defaultContentType(MediaType.APPLICATION_JSON);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
