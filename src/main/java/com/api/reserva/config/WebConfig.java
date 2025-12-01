78bb9be4a1a39bddbf4d5adc0cb71d8295a9e23c
Claudemir Teixeira Vitorino <teixeiravitorinoc8@gmail.com>
2025-12-01 16:48:48 -0300

    Add additional allowed origins for CORS
    Signed-off-by: Claudemir Teixeira Vitorino <teixeiravitorinoc8@gmail.com>

--- src/main/java/com/api/reserva/config/WebConfig.java
+++ src/main/java/com/api/reserva/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://espaco-senai.vercel.app", "https://espaco-senai.vercel.app/")
+                        .allowedOrigins("http://localhost:5173", "https://espaco-senai.vercel.app", "https://espaco-senai.vercel.app/", "http://localhost:19000", "http://localhost:19001", "exp://localhost:19000", "http://localhost:8081")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
