18585dbcfcb67e3908fbd325f261ad66c5ea8280
Claudemir Teixeira Vitorino <teixeiravitorinoc8@gmail.com>
2025-12-11 15:12:15 -0300

    Add 'IMPRESSORA' role to data.sql
    Signed-off-by: Claudemir Teixeira Vitorino <teixeiravitorinoc8@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 insert ignore into tb_roles VALUES (1, 'ADMIN'),
                                    (2, 'COORDENADOR'),
                                    (3, 'PROFESSOR'),
-                                   (4, 'ESTUDANTE');
+                                   (4, 'ESTUDANTE'),
+                                   (5, 'IMPRESSORA');
